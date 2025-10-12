46c48ba1d0029653201bd3bda93bd6921f837556
LongLiver69 <longliv2k2@gmail.com>
2025-10-12 17:27:56 +0700

    add layout

--- apps/repo_host/src/app/app.ts
+++ apps/repo_host/src/app/app.ts
@@ -1,3 +1,4 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { KeycloakService } from 'keycloak-angular';
@@ -5,9 +6,10 @@ import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
-  imports: [RouterModule, NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule],
+  imports: [RouterModule, NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule, NzInputModule, NgTemplateOutlet],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
@@ -24,4 +26,77 @@ export class App {
   logout() {
     this.keycloakService.logout(window.location.origin + '/');
   }
+
+  menus = [
+    {
+      level: 1,
+      title: 'Mail Group',
+      icon: 'mail',
+      open: true,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'Group 1',
+          icon: 'bars',
+          open: false,
+          selected: false,
+          disabled: false,
+          children: [
+            {
+              level: 3,
+              title: 'Option 1',
+              selected: false,
+              disabled: false
+            },
+            {
+              level: 3,
+              title: 'Option 2',
+              selected: false,
+              disabled: true
+            }
+          ]
+        },
+        {
+          level: 2,
+          title: 'Group 2',
+          icon: 'bars',
+          selected: true,
+          disabled: false
+        },
+        {
+          level: 2,
+          title: 'Group 3',
+          icon: 'bars',
+          selected: false,
+          disabled: false
+        }
+      ]
+    },
+    {
+      level: 1,
+      title: 'Team Group',
+      icon: 'team',
+      open: false,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'User 1',
+          icon: 'user',
+          selected: false,
+          disabled: false
+        },
+        {
+          level: 2,
+          title: 'User 2',
+          icon: 'user',
+          selected: false,
+          disabled: false
+        }
+      ]
+    }
+  ];
 }
