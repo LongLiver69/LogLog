dc2c4d75573d765cd077e25d2ec07d6fc69042fd
LongLiv69 <longliv2k2@gmail.com>
2026-01-13 16:11:20 +0700

    fix success signalr

--- apps/repo_feed/webpack.config.ts
+++ apps/repo_feed/webpack.config.ts
@@ -6,4 +6,33 @@ import config from './module-federation.config';
  * The DTS Plugin can be enabled by setting dts: true
  * Learn more about the DTS Plugin here: https://module-federation.io/configure/dts.html
  */
-export default withModuleFederation(config, { dts: false });
+export default withModuleFederation(config, {
+  dts: false,
+}).then((mfConfig: any) => {
+  return {
+    ...mfConfig,
+    resolve: {
+      ...(mfConfig.resolve || {}),
+      fallback: {
+        ...(mfConfig.resolve?.fallback || {}),
+        // Prevent Node.js built-in modules from being bundled (fixes @microsoft/signalr issues)
+        fs: false,
+        path: false,
+        os: false,
+        crypto: false,
+        stream: false,
+        util: false,
+        events: false,
+        buffer: false,
+        process: false,
+        http: false,
+        https: false,
+        zlib: false,
+        assert: false,
+        url: false,
+        module: false,
+      },
+    },
+  };
+});
+
--- apps/repo_host/module-federation.config.ts
+++ apps/repo_host/module-federation.config.ts
@@ -1,9 +1,9 @@
 import { ModuleFederationConfig } from '@nx/module-federation';
 
 const config: ModuleFederationConfig = {
-  name: 'repo_host',  
+  name: 'repo_host',
   remotes: [
-    'repo_chat', 
+    'repo_chat',
     'repo_feed'
   ],
   shared: (libraryName, defaultConfig) => {
@@ -12,7 +12,6 @@ const config: ModuleFederationConfig = {
         singleton: true,
         strictVersion: false,
         requiredVersion: false,
-        eager: true,
       };
     }
     return defaultConfig;
